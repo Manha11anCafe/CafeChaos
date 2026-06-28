d8c59fb65c6c5e303c68127ffe952042df723888
CΛF3 <mafiaworkemail@gmail.com>
2026-06-28 13:58:39 +0700

    feat: add customer class

---
+++ src/ReplicatedStorage/Modules/Customer/Customer.lua
@@ -0,0 +1,17 @@
+local Customer = {}
+Customer.__index = Customer
+
+function Customer.new(id)
+	local self = setmetatable({}, Customer)
+
+	self.Id = id
+	self.State = "Idle"
+	self.Order = nil
+	self.Patience = 100
+	self.Mood = "Neutral"
+	self.Reward = 0
+
+	return self
+end
+
+return Customer
