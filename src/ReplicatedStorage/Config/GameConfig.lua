7cdbce165c3e624bd379e4598fd20b740ee2759d
CΛF3 <mafiaworkemail@gmail.com>
2026-06-28 13:35:09 +0700

    feat: add shared game config

---
+++ src/ReplicatedStorage/Config/GameConfig.lua
@@ -0,0 +1,10 @@
+local GameConfig = {}
+
+GameConfig.Debug = true
+
+GameConfig.Game = {
+	Name = "CafeChaos",
+	Version = "0.1.0"
+}
+
+return GameConfig
--- src/ServerScriptService/Boot/Bootstrap.server.lua
+++ src/ServerScriptService/Boot/Bootstrap.server.lua
@@ -1,8 +1,11 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
-local Logger = require(
-	ReplicatedStorage.Modules.Shared.Logger
-)
+local Logger = require(ReplicatedStorage.Modules.Shared.Logger)
+local GameConfig = require(ReplicatedStorage.Config.GameConfig)
 
 Logger:Info("Bootstrap", "Server is starting...")
+Logger:Info(
+	"Bootstrap",
+	string.format("%s v%s", GameConfig.Game.Name, GameConfig.Game.Version)
+)
 Logger:Info("Bootstrap", "Initialization complete.")
